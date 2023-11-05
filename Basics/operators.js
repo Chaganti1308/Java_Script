7eb19eae2659b056733abd6e00a95fea484f086b
Chaganti1308 <93071195+Chaganti1308@users.noreply.github.com>
2023-11-05 16:51:40 +0000

    about operators

---
+++ Basics/operators.js
@@ -0,0 +1,33 @@
+// Arithmetic Operators
+// +,-,/,*
+
+const numberOne = "2"
+const numberTwo = "4"
+
+const numberThree = numberOne + numberTwo
+
+console.log(numberThree);  // output : 24 -- string concatenation
+
+
+
+const strOne = "Hello.."
+const strTwo = "Neeha"
+
+const strThree = strOne + strTwo
+
+console.log(strThree);  // output : Hello..Neeha -- string concatenation
+
+let x = 10
+let y = 20
+
+let z = x + y
+
+console.log(z);  // output : 30 --- addition
+
+
+// Assignment operators
+let xval = 10
+xval += 120
+console.log(xval);
+
+
