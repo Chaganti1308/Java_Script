708fe41dce9de26f20aeeb62b78f03614054a9e1
Chaganti1308 <93071195+Chaganti1308@users.noreply.github.com>
2023-11-18 12:07:50 +0530

    Delete Basics directory

--- Basics/numAndString.js
+++
@@ -1,7 +0,0 @@
-const firstName = "Neeharika"
-const lastName = 'Chaganti'
-
-console.log(firstName + lastName);
-console.log(`My first name ${firstName} is and my lastname is ${lastName} `);
-
-// const fName = new String("Neeha") -- actual string assignment
