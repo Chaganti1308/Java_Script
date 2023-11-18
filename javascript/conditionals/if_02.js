e4ab806d2fa18a07b379f5679fed3ffcf7c61ffe
Chaganti1308 <93071195+Chaganti1308@users.noreply.github.com>
2023-11-18 05:39:12 +0000

    if block

---
+++ javascript/conditionals/if_02.js
@@ -0,0 +1,34 @@
+// const score = 99
+
+// console.log("Welcome to the game"); 
+
+// if (score > 99 ){
+//     console.log("You will get a life up");
+// }
+// else {
+//     console.log("You don't get it");
+// }
+
+// if the score is above 100, you can less the score by 100
+// you can increase the life by 1 after score reaches to 100
+
+// let score = 110
+// let life = 3
+// let bullets = 1000
+// console.log("Welcome to the game"); 
+
+// if (score > 99 ){
+//     console.log("You will get a life up");
+//     score = score - 100
+//     life = life + 1
+//     // let bullets = 2000
+//     bullets = 2000
+//     console.log(`bullets are ${bullets}`);
+
+// }
+// else {
+//     console.log("You don't get it");
+// }
+
+// console.log(`You have score of ${score} and and life count is ${life} and bullets are ${bullets}`);
+
