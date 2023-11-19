65501f1869b80f8d782009e21c4491b392ff7766
Chaganti1308 <93071195+Chaganti1308@users.noreply.github.com>
2023-11-19 05:35:20 +0000

    using ternary operator

---
+++ javascript/conditionals/driving_licence.js
@@ -0,0 +1,14 @@
+// let citizenAge = 19
+
+// if(citizenAge>=18){
+//     console.log("Eligible to get driving licence");
+// }
+// else{
+//     console.log("Nor eligible to get driving licence");
+// }
+
+// ternary operator
+let citizenAge = 19
+let getDrivingLicence = (citizenAge >= 18 )?"Eligible to get driving licence":"Not eligible to get driving licence"
+
+console.log(getDrivingLicence);
