25494cbae7dbee7dbff5b328d097f2163d3377de
Chaganti1308 <93071195+Chaganti1308@users.noreply.github.com>
2023-12-19 07:16:26 +0000

    callback and Higher order function

--- javascript/functions/advanced_js_functions/callbacks.js
+++ javascript/functions/advanced_js_functions/callbacks.js
@@ -4,10 +4,15 @@
  */
 function processArray(array, fun){
     for(let i = 0; i< array.length; i++) // iterating over every index of an array
-        console.log("index " ,i, " value", array[i], "return cube value of function" , fun(array[i]));
+        console.log("index " ,i, " value", array[i], "return  value of function" , fun(array[i]));
         // i -> inedx
         // array[i] -> value at index i
         // passing value of array[i] to the fun-> cube
 }
 
-processArray([1,2,3,4,5,6],function cube(x) { return x*x*x ;}); // passing function as arguement fun -> cube
+// processArray([1,2,3,4,5,6],function cube(x) { return x*x*x ;}); // passing function as arguement fun -> cube
+
+processArray([1,2,3,4,5,6],function square(x) { return x*x ;}); // passing function as arguement fun -> square
+
+// square and cube functions are callback function
+// processArray is the Higher Order Function
